bb3d296940db0273383c4991457f58829053bbb8
panucci <eyakubu442@gmail.com>
2025-02-24 19:30:02 +0100

    added few styles

--- js/index.js
+++ js/index.js
@@ -21,9 +21,8 @@ const renderPosts = async (term) => {
   posts.forEach(post => {
     template += `
       <div class="post">
-        <h2>${post.title}</h2>
-        <p><small>${post.likes} likes</small></p>
-        <p>${post.body.slice(0, 200)}...</p>
+        <h2><span>${post.id}) </span>${post.title}</h2>
+        <p>${post.body.slice(0, 50)}...</p>
         <a href="details.html?id=${post.id}">Read more</a>
       </div>
     `
